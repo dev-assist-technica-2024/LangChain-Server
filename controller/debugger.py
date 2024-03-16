3c926abd2da42b95bb5fa61a862379b5e14678c6
soosan-shabnam <soosan2609@gmail.com>
2024-03-16 05:31:43 +0530

    debugger and optimizer commented out

--- controller/debugger.py
+++ controller/debugger.py
@@ -8,43 +8,43 @@ load_dotenv()
 
 
 client = OpenAI()
-assistant = client.beta.assistants.create(
-    name="Code Debugger",
-    instructions="You are a code debugger. You will be given an entire codebase along with an error "
-                 "stacktrace or"
-                 "user query or both. You will have to identify where the error is happening and give the "
-                 "solution."
+# assistant = client.beta.assistants.create(
+#     name="Code Debugger",
+#     instructions="You are a code debugger. You will be given an entire codebase along with an error "
+#                  "stacktrace or"
+#                  "user query or both. You will have to identify where the error is happening and give the "
+#                  "solution."
+#
+#                  "You will be given the codebase in the format:"
+#
+#                  "Codebase:"
+#                  "{File Path}"
+#                  "//Code Start"
+#                  "{Code}"
+#                  "//Code End"
+#
+#                  "Everything after the Codebase label are the different files with their file path and code. "
+#                  "The files denoting code will have //Code Start and //Code End to denote the code. There "
+#                  "will be configuration files there as well and they wont have the code identifiers so you "
+#                  "will know which ones are the config files."
+#
+#                  "After this, you will be given an error stacktrace or the user query. It will be in the "
+#                  "format:"
+#
+#                  "User Query:"
+#                  "{Error}"
+#
+#                  "In the error field it will either be some sort of query regarding the code or a stacktrace"
+#
+#                  "Given all this you will figure out where the error is happening, why the error is happening "
+#                  "and give the solution with code if necessary. Explain in utmost detail. If"
+#                  "you don't know the answer, say you don't know.",
+#
+#     tools=[{"type": "code_interpreter"}],
+#     model="gpt-4-turbo-preview",
+# )
 
-                 "You will be given the codebase in the format:"
-
-                 "Codebase:"
-                 "{File Path}"
-                 "//Code Start"
-                 "{Code}"
-                 "//Code End"
-
-                 "Everything after the Codebase label are the different files with their file path and code. "
-                 "The files denoting code will have //Code Start and //Code End to denote the code. There "
-                 "will be configuration files there as well and they wont have the code identifiers so you "
-                 "will know which ones are the config files."
-
-                 "After this, you will be given an error stacktrace or the user query. It will be in the "
-                 "format:"
-
-                 "User Query:"
-                 "{Error}"
-
-                 "In the error field it will either be some sort of query regarding the code or a stacktrace"
-
-                 "Given all this you will figure out where the error is happening, why the error is happening "
-                 "and give the solution with code if necessary. Explain in utmost detail. If"
-                 "you don't know the answer, say you don't know.",
-
-    tools=[{"type": "code_interpreter"}],
-    model="gpt-4-turbo-preview",
-)
-
-os.environ["DEBUGGER_ID"] = assistant.id
+# os.environ["DEBUGGER_ID"] = assistant.id
 
 
 async def initialize_thread_debugger():
--- controller/optimizer.py
+++ controller/optimizer.py
@@ -6,46 +6,46 @@ import os
 load_dotenv()
 
 client = OpenAI()
-assistant = client.beta.assistants.create(
-    name="Code Optimizer",
-    instructions="You are a code optimizer. You will be given a codebase that you need to optimize so that it is "
-                 "efficient in terms of time and space complexity. It should also be modular and follow the standard "
-                 "design"
-                 "and security principles. Other than this, The user might have specific requests regarding the "
-                 "nature of optimization so that should be given more weight and the entire code should be optimized"
-                 "around that."
-
-                 "You will be given the code in the format:"
-
-                 "Codebase:"
-                 "{File Path}"
-                 "//Code Start"
-                 "{Code}"
-                 "//Code End"
-                 
-                 "Everything after the Codebase label are the different files with their file path and code. "
-                 "The files denoting code will have //Code Start and //Code End to denote the code. There "
-                 "will be configuration files there as well and they wont have the code identifiers so you "
-                 "will know which ones are the config files."
-
-                 "After this, you will be given the user query. It will be in the "
-                 "format:"
-
-                 "User Query:"
-                 "{Request}"
-
-                 "In the request field it will either be some sort of specific request about the optimization"
-
-                 "Given all this you will look at the entire codebase and optimize any code as best as you can "
-                 "keeping the user's requirements in mind. You might have to optimize multiple files or one file. It"
-                 "depends entirely on the user's request"
-                 "Explain in utmost detail every step you took. If you can't optimize, say you don't know",
-
-    tools=[{"type": "code_interpreter"}],
-    model="gpt-4-turbo-preview",
-)
-
-os.environ["OPTIMIZER_ID"] = assistant.id
+# assistant = client.beta.assistants.create(
+#     name="Code Optimizer",
+#     instructions="You are a code optimizer. You will be given a codebase that you need to optimize so that it is "
+#                  "efficient in terms of time and space complexity. It should also be modular and follow the standard "
+#                  "design"
+#                  "and security principles. Other than this, The user might have specific requests regarding the "
+#                  "nature of optimization so that should be given more weight and the entire code should be optimized"
+#                  "around that."
+#
+#                  "You will be given the code in the format:"
+#
+#                  "Codebase:"
+#                  "{File Path}"
+#                  "//Code Start"
+#                  "{Code}"
+#                  "//Code End"
+#
+#                  "Everything after the Codebase label are the different files with their file path and code. "
+#                  "The files denoting code will have //Code Start and //Code End to denote the code. There "
+#                  "will be configuration files there as well and they wont have the code identifiers so you "
+#                  "will know which ones are the config files."
+#
+#                  "After this, you will be given the user query. It will be in the "
+#                  "format:"
+#
+#                  "User Query:"
+#                  "{Request}"
+#
+#                  "In the request field it will either be some sort of specific request about the optimization"
+#
+#                  "Given all this you will look at the entire codebase and optimize any code as best as you can "
+#                  "keeping the user's requirements in mind. You might have to optimize multiple files or one file. It"
+#                  "depends entirely on the user's request"
+#                  "Explain in utmost detail every step you took. If you can't optimize, say you don't know",
+#
+#     tools=[{"type": "code_interpreter"}],
+#     model="gpt-4-turbo-preview",
+# )
+#
+# os.environ["OPTIMIZER_ID"] = assistant.id
 
 
 async def initialize_thread_optimizer():
